191da2f9e03ee48720a7d091786c1764872b334f
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-15 23:00:30 -0300

    feat: add context menu xml

--- app/src/main/java/com/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/personaltasks/ui/MainActivity.kt
@@ -1,21 +1,49 @@
 package com.personaltasks.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.personaltasks.R
+import com.personaltasks.databinding.ActivityMainBinding
+import com.personaltasks.model.Constant.EXTRA_TASK
+import com.personaltasks.model.Task
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val taskList: MutableList<Task> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        setSupportActionBar(amb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Personal tasks"
+
+        carl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    result.data?.getParcelableExtra(EXTRA_TASK, Task::class.java)
+                }
+                else {
+                    result.data?.getParcelableExtra<Task>(EXTRA_TASK)
+                }
+                task?.let{ receivedTask ->
+                    val position = taskList.indexOfFirst { it.id == receivedTask.id }
+                    if (position == -1) {
+                        taskList.add(receivedTask)
+
+                    }
+                }
+            }
         }
     }
 }
