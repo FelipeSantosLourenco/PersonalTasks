8f573c7a0dfcf1725657f9661ac3dc98b7341405
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-16 01:10:02 -0300

    feat: add main controller functions - insertTask - updateTask - removeTask - getTask - getTasks

--- app/src/main/java/com/personaltasks/controller/MainController.kt
+++ app/src/main/java/com/personaltasks/controller/MainController.kt
@@ -1,6 +1,24 @@
 package com.personaltasks.controller
 
+import androidx.room.Room
+import com.personaltasks.model.Task
+import com.personaltasks.model.TaskDao
+import com.personaltasks.model.TaskRoomDb
 import com.personaltasks.ui.MainActivity
 
 class MainController(mainActivity: MainActivity) {
+    private val taskDao: TaskDao = Room.databaseBuilder(
+        mainActivity, TaskRoomDb::class.java, "task-database"
+    ).build().taskDao()
+
+    fun insertTask(task: Task){
+        Thread {
+            taskDao.createTask(task)
+        }.start()
+    }
+
+    fun getTask(id: Int) = taskDao.retrieveTask(id)
+    fun getTasks() = taskDao.retrieveTasks()
+    fun updateTask(task: Task) = taskDao.updateTask(task)
+    fun removeTask(task: Task) = taskDao.deleteTask(task)
 }
