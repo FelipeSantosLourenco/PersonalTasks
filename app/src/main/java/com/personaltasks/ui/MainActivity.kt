78372bf48ff3ca9186360c0c90a520780ade716c
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-15 23:41:26 -0300

    feat: onCreate function, set adapter and mainController on MainActivity

--- app/src/main/java/com/personaltasks/ui/MainActivity.kt
+++ app/src/main/java/com/personaltasks/ui/MainActivity.kt
@@ -1,25 +1,39 @@
 package com.personaltasks.ui
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.personaltasks.R
+import com.personaltasks.adapter.TaskAdapter
 import com.personaltasks.databinding.ActivityMainBinding
 import com.personaltasks.model.Constant.EXTRA_TASK
 import com.personaltasks.model.Task
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnTaskClickListener {
 
     private val amb: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
 
     private val taskList: MutableList<Task> = mutableListOf()
+
+    private val taskAdapter: TaskAdapter by lazy {
+        TaskAdapter(taskList, this)
+    }
+
+    private lateinit var carl:ActivityResultLauncher<Intent>
+
+    private val mainController: MainController by lazy {
+        MainController(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +54,42 @@ class MainActivity : AppCompatActivity() {
                     val position = taskList.indexOfFirst { it.id == receivedTask.id }
                     if (position == -1) {
                         taskList.add(receivedTask)
-
+                        taskAdapter.notifyItemInserted(taskList.lastIndex)
+                        mainController.insertTask(receivedTask)
+                    }
+                    else{
+                        taskList[position] = receivedTask
+                        taskAdapter.notifyItemChanged(position)
+                        mainController.modifyTask(receivedTask)
                     }
                 }
             }
         }
+
+        amb.taskRv.adapter = taskAdapter
+        amb.taskRv.layoutManager = LinearLayoutManager(this)
+
+        fillTaskList()
+    }
+
+    override fun onTaskClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRemoveTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onEditTaskMenuItemClick(position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun fillTaskList() {
+        taskList.clear()
+
+        Thread {
+            taskList.addAll(mainController.getTasks())
+            taskAdapter.notifyDataSetChanged()
+        }.start()
     }
 }
