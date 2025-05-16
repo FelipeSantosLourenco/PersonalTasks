b14fd75036dc4c6ba060e123047f99de61390d8b
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-16 00:52:34 -0300

    feat: add entity annotation

--- app/src/main/java/com/personaltasks/model/Task.kt
+++ app/src/main/java/com/personaltasks/model/Task.kt
@@ -1,11 +1,13 @@
 package com.personaltasks.model
 
 import android.os.Parcelable
+import androidx.room.Entity
 import com.personaltasks.model.Constant.INVALID_TASK_ID
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 
 @Parcelize
+@Entity
 data class Task (
     var id: Int? = INVALID_TASK_ID,
     var title: String = "",
