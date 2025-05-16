7aa54ffe0a8d7a0ab09ef9426934b2bfd75a8d2c
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-16 16:32:28 -0300

    feat: implement onCreate method in TaskActivity - inflate the layout - retrieve the task from the intent - checks if the screen is for viewing only, if so, disable the edit text - put the task in the result intent and finish the activity

--- app/src/main/java/com/personaltasks/ui/TaskActivity.kt
+++ app/src/main/java/com/personaltasks/ui/TaskActivity.kt
@@ -1,6 +1,74 @@
 package com.personaltasks.ui
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.personaltasks.databinding.ActivityTaskBinding
+import com.personaltasks.model.Constant.EXTRA_TASK
+import com.personaltasks.model.Constant.EXTRA_VIEW_TASK
+import com.personaltasks.model.Task
+import java.time.LocalDate
 
 class TaskActivity : AppCompatActivity() {
+
+    private val atb: ActivityTaskBinding by lazy {
+        ActivityTaskBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(atb.root)
+
+        setSupportActionBar(atb.toolbarIn.toolbar)
+        supportActionBar?.subtitle = "Nova task"
+
+        val receivedTask = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent.getParcelableExtra(EXTRA_TASK, Task::class.java)
+        }
+        else {
+            intent.getParcelableExtra<Task>(EXTRA_TASK)
+        }
+
+        receivedTask?.let {
+            supportActionBar?.subtitle = "Editar task"
+            with(atb) {
+                tituloEt.setText(it.title)
+                descricaoEt.setText(it.description)
+
+                // datePicker usa index de 0 a 11, então precisa tirar 1
+                dataLimiteEt.updateDate(it.date.year, it.date.monthValue - 1, it.date.dayOfMonth)
+
+                // verifica se é somente vizualização
+                val viewTask = intent.getBooleanExtra(EXTRA_VIEW_TASK, false)
+                if (viewTask) {
+                    supportActionBar?.subtitle = "Visualizar task"
+                    tituloEt.isEnabled = false
+                    descricaoEt.isEnabled = false
+                    dataLimiteEt.isEnabled = false
+                    salvarBt.visibility = View.GONE
+                }
+            }
+        }
+
+        with(atb) {
+            salvarBt.setOnClickListener{
+                Task(
+                    // se recebeu a task, matém o ID, senão gera um novo
+                    receivedTask?.id?:hashCode(),
+                    tituloEt.text.toString().trim(),
+                    descricaoEt.text.toString().trim(),
+                    // datePicker usa index de 0 a 11 para mês
+                    LocalDate.of(dataLimiteEt.year, dataLimiteEt.month + 1, dataLimiteEt.dayOfMonth)
+                ).let { task ->
+                    Intent().apply {
+                        putExtra(EXTRA_TASK, task)
+                        setResult(RESULT_OK, this)
+                    }
+                }
+                finish()
+            }
+        }
+    }
 }
