df6242c91df3d799f5c9248c8f80eaeaa4b6c5a5
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-16 17:01:47 -0300

    feat: autogenerate ID on Task

--- app/src/main/java/com/personaltasks/model/Task.kt
+++ app/src/main/java/com/personaltasks/model/Task.kt
@@ -2,6 +2,7 @@ package com.personaltasks.model
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.personaltasks.model.Constant.INVALID_TASK_ID
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
@@ -9,7 +10,7 @@ import java.time.LocalDate
 @Parcelize
 @Entity
 data class Task (
-    var id: Int? = INVALID_TASK_ID,
+    @PrimaryKey(autoGenerate = true) var id: Int? = INVALID_TASK_ID,
     var title: String = "",
     var description: String = "",
     var date: LocalDate = LocalDate.now()
