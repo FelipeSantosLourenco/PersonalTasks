720d8e3002d9ea568aedacd6c82e93eccb1fa0d6
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-05-15 22:46:40 -0300

    feat: add OnTaskClickListener interface

---
+++ app/src/main/java/com/personaltasks/ui/OnTaskClickListener.kt
@@ -0,0 +1,8 @@
+package com.personaltasks.ui
+
+sealed interface OnTaskClickListener {
+    fun onTaskClick(position: Int)
+
+    fun onRemoveTaskMenuItemClick(position: Int)
+    fun onEditTaskMenuItemClick(position: Int)
+}
