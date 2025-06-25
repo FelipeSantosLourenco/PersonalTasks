8961873550202e98eee9fa4bcb68e0b4d980a133
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-25 15:27:54 -0300

    feat: add signIn click listener to LoginActivity

--- app/src/main/java/com/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/personaltasks/ui/LoginActivity.kt
@@ -2,10 +2,14 @@ package com.personaltasks.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.personaltasks.databinding.ActivityLoginBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     private val alb: ActivityLoginBinding by lazy {
@@ -25,5 +29,27 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
+        alb.signInBt.setOnClickListener {
+            signInCoroutineScope.launch {
+                Firebase.auth.signInWithEmailAndPassword(
+                    alb.emailLoginEt.text.toString(),
+                    alb.passwordLoginEt.text.toString()
+                ).addOnFailureListener {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "O login falhou. Causa: ${it.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }.addOnSuccessListener { openMainActivity() }
+            }
+        }
+
+    }
+
+    private fun openMainActivity() {
+        startActivity(
+            Intent(this@LoginActivity, MainActivity::class.java)
+        )
+        finish()
     }
 }
