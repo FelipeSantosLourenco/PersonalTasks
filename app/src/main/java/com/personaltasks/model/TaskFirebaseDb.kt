9607ab86fc4587bb69d2903b79d9628c4f47639e
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-24 21:22:18 -0300

    feat: add onChildAdded fun

--- app/src/main/java/com/personaltasks/model/TaskFirebaseDb.kt
+++ app/src/main/java/com/personaltasks/model/TaskFirebaseDb.kt
@@ -1,4 +1,25 @@
 package com.personaltasks.model
 
-class TaskFirebaseDb {
+import com.google.firebase.Firebase
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
+
+class TaskFirebaseDb: TaskDao {
+    private val databaseReference = Firebase.database.getReference("tasks")
+    private val tasks = mutableListOf<Task>()
+
+    init {
+        databaseReference.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val task = snapshot.getValue<Task>()
+                task?.let { newTask ->
+                    if (!tasks.any() { it.id == newTask.id})
+                        tasks.add(newTask)
+                }
+            }
+        })
+    }
+
 }
