f5699fb05c67c0a361a0cb37d0fde6c07acd1253
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-25 15:35:42 -0300

    feat: add signUp click listener on RegisterActivity

--- app/src/main/java/com/personaltasks/ui/RegisterActivity.kt
+++ app/src/main/java/com/personaltasks/ui/RegisterActivity.kt
@@ -1,12 +1,18 @@
 package com.personaltasks.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.personaltasks.R
 import com.personaltasks.databinding.ActivityRegisterBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity() {
     private val arb: ActivityRegisterBinding by lazy {
@@ -20,7 +26,29 @@ class RegisterActivity : AppCompatActivity() {
         setSupportActionBar(arb.toolbarIn.toolbar)
         supportActionBar?.subtitle = getString(R.string.cadastrar)
 
+        arb.signUpBt.setOnClickListener {
+            val signUpCoroutineScope = CoroutineScope(Dispatchers.IO)
 
+            signUpCoroutineScope.launch {
+                Firebase.auth.createUserWithEmailAndPassword(
+                    arb.emailRegisterEt.text.toString(),
+                    arb.passwordRegisterEt.text.toString()
+                ).addOnFailureListener {
+                    Toast.makeText(
+                        this@Regis.terActivity,
+                        "Cadastro falhou. Causa: ${it.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }.addOnSuccessListener {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Cadastrado com sucesso!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                }
+            }
+        }
 
     }
 }
