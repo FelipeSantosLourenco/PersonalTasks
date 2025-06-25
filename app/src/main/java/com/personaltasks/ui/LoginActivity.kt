33c8a384632d9a17cdf4717d0f75d21047d81753
Felipe Santos Lourenço <127883811+fezinnn@users.noreply.github.com>
2025-06-25 15:29:35 -0300

    feat: add resetPassword click listener

--- app/src/main/java/com/personaltasks/ui/LoginActivity.kt
+++ app/src/main/java/com/personaltasks/ui/LoginActivity.kt
@@ -44,6 +44,15 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        alb.resetPasswordBt.setOnClickListener {
+            signInCoroutineScope.launch {
+                val email = alb.emailLoginEt.text.toString()
+                if (email.isNotEmpty()) {
+                    Firebase.auth.sendPasswordResetEmail(email)
+                }
+            }
+        }
+
     }
 
     private fun openMainActivity() {
